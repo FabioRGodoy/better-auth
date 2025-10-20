93d38bad14c9e8ae0fdad41595436879c45e3744
FabioRGodoy <fabiogodoy955@icloud.com>
2025-10-20 19:44:36 -0300

    chore: add pnpm workspace configuration and turbo.json for task management

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="w-full min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-lg text-white shadow-2xl font-bold">Hello World!</h1>
+    </main>
+  );
+}
